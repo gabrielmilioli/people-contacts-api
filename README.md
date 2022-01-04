19b84e71f970c91b7699364ce0e905ddbd37bc36
Gabriel <gmilioli@outlook.com>
2022-01-04 18:14:17 -0300

    aJUSTES

---
+++ README.md
@@ -0,0 +1,21 @@
+# Projeto de pessoas e contatos com Nodejs
+
+### Instalação
+```
+npm install
+```
+
+### Crie um schema via MySQL
+```
+CREATE SCHEMA innova;
+```
+
+### Execute os migrations
+```
+npm run migrations
+```
+
+### Execute o projeto
+```
+npm run develop
+```
--- src/migrations/1641329681748-migration.ts
+++ src/migrations/1641329681748-migration.ts
@@ -1,12 +1,12 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class migration1641256860745 implements MigrationInterface {
-    name = 'migration1641256860745'
+export class migration1641329681748 implements MigrationInterface {
+    name = 'migration1641329681748'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("CREATE TABLE `person` (`id` int NOT NULL AUTO_INCREMENT, `name` varchar(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
         await queryRunner.query("CREATE TABLE `contact` (`id` int NOT NULL AUTO_INCREMENT, `value` varchar(255) NOT NULL DEFAULT '', `type` tinyint NOT NULL DEFAULT 1, `id_person` int NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
-        await queryRunner.query("ALTER TABLE `contact` ADD CONSTRAINT `FK_5d0a5598bf0d6b386b8d8578a1e` FOREIGN KEY (`id_person`) REFERENCES `person`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
+        await queryRunner.query("ALTER TABLE `contact` ADD CONSTRAINT `FK_5d0a5598bf0d6b386b8d8578a1e` FOREIGN KEY (`id_person`) REFERENCES `person`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
